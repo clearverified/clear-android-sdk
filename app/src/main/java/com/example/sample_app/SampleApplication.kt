78f3c275d119575c6b8d69c4d24cdd65c61bcfc1
Rashad C <rashad.cureton@clearme.com>
2021-12-10 10:32:54 -0500

    added sample project with skeleton code

--- app/src/main/java/com/example/sample_app/SampleApplication.kt
+++ app/src/main/java/com/example/sample_app/SampleApplication.kt
@@ -1,4 +1,19 @@
 package com.example.sample_app
 
-class SampleApplication {
+import android.app.Application
+import com.clearme.verification.sdk.ClearEnvironment.INTEGRATION
+import com.clearme.verification.sdk.ClearSdk
+
+class SampleApplication : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+    ClearSdk.initialize(
+      application = this,
+      clientId = "YOUR CLIENT ID",
+      apiKey = "YOUR API KEY" ,
+      deviceVerificationKey = "YOUR DEVICE VERIFICATION KEY",
+      environment = INTEGRATION
+    )
+  }
 }
